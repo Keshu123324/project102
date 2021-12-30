705186cc0f516a492a683144990468da2f064bf8
Keshu123324 <74176491+Keshu123324@users.noreply.github.com>
2021-12-30 19:34:16 +0530

    Add files via upload

---
+++ project102.py
@@ -0,0 +1,21 @@
+print("How to make a chocolate cake")
+
+print("Grease the pans")
+
+print("Prepare the ingredients and chocolate")
+
+print("Mix dry ingredients")
+
+print("Cream the butter and sugar")
+
+print("Add chocolate")
+
+print("Add flour and milk")
+
+print("Put batter in pans")
+
+print("Bake and cool the cake...")
+
+print("decorate it with cream the way you like")
+
+print("Your cake is ready:)")
